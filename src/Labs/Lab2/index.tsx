7959981e80555dba764f836105dceebefd250a7d
novakoki <shaoziqi0628@gmail.com>
2025-07-13 18:11:22 -0400

    a2 2.2 2.3

--- src/Labs/Lab2/index.tsx
+++ src/Labs/Lab2/index.tsx
@@ -11,9 +11,17 @@ import ZIndex from "./ZIndex";
 import Float from "./Float";
 import GridLayout from "./GridLayout";
 import Flex from "./Flex";
+import ReactIcons from "./ReactIcons";
+import { Container } from "react-bootstrap";
+import BootstrapGrids from "./BootstrapGrids";
+import ScreenSizeLabel from "./ScreenSizeLabel";
+import BootstrapTables from "./BootstrapTables";
+import BootstrapLists from "./BootstrapLists";
+import BootstrapForms from "./BootstrapForms";
+import BootstrapNavigation from "./BootstrapNavigation";
 export default function Lab2() {
   return (
-    <div id="wd-lab2">
+    <Container>
       <h2>Lab 2 - Cascading Style Sheets</h2>
       <h3>Styling with the STYLE attribute</h3>
       <p>
@@ -76,6 +84,13 @@ export default function Lab2() {
 <Float />
 <GridLayout />
 <Flex />
-    </div>
+<ReactIcons />
+<BootstrapGrids />
+<ScreenSizeLabel />
+<BootstrapTables />
+<BootstrapLists />
+<BootstrapForms />
+<BootstrapNavigation />
+    </Container>
   );
 }
