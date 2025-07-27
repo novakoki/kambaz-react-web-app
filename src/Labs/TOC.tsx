9fab08c5c7d86afed63e4c0fae6a76a547be8afd
novakoki <shaoziqi0628@gmail.com>
2025-07-27 15:49:34 -0400

    4.1-4.3

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -16,6 +16,9 @@ export default function TOC() {
        <Nav.Link to="/Labs/Lab3" as={Link} active={pathname.includes("Lab3")} id="wd-a3">Lab 3</Nav.Link>
      </Nav.Item>
      <Nav.Item>
+       <Nav.Link to="/Labs/Lab4" as={Link} active={pathname.includes("Lab4")} id="wd-a4">Lab 4</Nav.Link>
+     </Nav.Item>
+     <Nav.Item>
        <Nav.Link to="/Kambaz" as={Link} id="wd-a3">Kambaz</Nav.Link>
      </Nav.Item>
      <Nav.Item>
